272a536bd8a5ed51da33a030392249c4223070fc
6D157 <6d157@kanrishanoiMac.local>
2021-11-01 17:03:30 +0900

    add

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <div class="aa">
+    <div class="form-group">
+        <div class="input-group">
+            <input type="text" v-model="newItem" class="form-control">
+            <span class="input-group-btn"><button class="btn btn-primary" @keydown.enter="onKeyDown()" @click="addItem()">登録</button></span>
+            <!-- <input @name.enter="addItem()"> -->
+            <span class="input-group-btn"><button class="btn btn-primary1" @click="shuffles()">シャッフルスタート！！</button></span>
+        </div>
+        <ul class="submit_group">
+          <li class="list_group" v-for="todo in todos" :key="todo">{{todo}}</li>
+        </ul>
+    </div>
+
+    <div id="zaseki">
+      <ul id="zaseki_name">
+        <li class="index_name" v-for="(name,index) in names" :key="name.id">
+          <div class="name">{{index + 1}}:</div>
+          <div class="name">{{name}}</div>
+        </li>
+      </ul>
+    </div>
   </div>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'Zaseki',
+  data() {
+    return {
+      // newItem: '',
+      // todos: [],
+      names: [
+      
+      ],
+      shuffle_names: [],
+
+    }
+  },
+    mounted: function() {
+      // this.shuffle_names = this.shuffle(this.names)
+  },
+    methods: {
+      addItem: function(){
+        this.names.push(this.newItem);
+        this.newItem = '';
+      },
+      shuffles() {
+        this.shuffle_names = this.shuffle(this.names)
+        this.shuffle_names.splice()
+      },
+      onKeyDown: function() {
+        this.names.push(this.newItem);
+        this.newItem = '';
+    },
+    shuffle: function(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        let r = Math.floor(Math.random() * (i + 1))
+        let tmp = array[i]
+        array[i] = array[r]
+        array[r] = tmp
+      }
+      return array
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* .aa {
+  display: flex;
+} */
+
+.index_name {
+  list-style: none;
+  font-size: 30px;
+  border: solid 1px;
+  display: flex;
+  padding-left: 10px;
+  padding-right: 10px;
+  width: 40%;
+  margin-left :250px ;
+}
+
+.name {
+  padding-left: 20px;
+  padding-right: 10px;
+}
+
+.btn-primary1 {
+  position: absolute;
+  padding:  10px;             /* 余白指定 */
+  left:  15px;                /* 左からの位置指定 */
+  top: 100px;
 }
 </style>
